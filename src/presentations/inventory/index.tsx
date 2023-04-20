83ca54d7204629822a126d5c18817efa852d4fe5
sopencody <o.sophin29@gmail.com>
2023-04-20 15:21:24 +0700

    fix

--- src/presentations/inventory/index.tsx
+++ src/presentations/inventory/index.tsx
@@ -8,37 +8,17 @@ import UsersModal from '@/components/modal/UsersModal';
 import DocumentNumberingModal from '@/components/modal/DocumentNumberingModal';
 
 export default function InventoryMasterPage() {
-    const [openBranch, setOpenBranch] = React.useState(false);
-    const [openDimension, setOpenDimension] = React.useState(false);
-    const [openUser, setOpenUser] = React.useState(false);
-    const [openDoc, setOpenDoc] = React.useState(false);
-
-
-    const previewBranch = React.useCallback(() => setOpenBranch(true), []);
-    const closeBranch = React.useCallback(() => setOpenBranch(false), []);
     
-    const previewDimension = React.useCallback(() => setOpenDimension(true), []);
-    const closeDimension = React.useCallback(() => setOpenDimension(false), []);
-
-    const previewUser = React.useCallback(() => setOpenUser(true), []);
-    const closeUser = React.useCallback(() => setOpenUser(false), []);
-
-    const previewDoc = React.useCallback(() => setOpenDoc(true), []);
-    const closeDoc = React.useCallback(() => setOpenDoc(false), []);
 
     return (
         <>
             <MainContainer title='Inventory'>
-                <ItemCard title='Branch' icon={<AiOutlineSolution />} onClick={previewBranch} />
-                <ItemCard title='Dimension' icon={<AiOutlineFileSync />} onClick={previewDimension} />
-                <ItemCard title='Document Numbering' icon={<AiOutlineFileExclamation />} onClick={previewDoc} />
+                <ItemCard title='Branch' icon={<AiOutlineSolution />}  />
+                <ItemCard title='Dimension' icon={<AiOutlineFileSync />} />
+                <ItemCard title='Document Numbering' icon={<AiOutlineFileExclamation />}  />
                 <ItemCard title='User Licensing' icon={<AiOutlineFileText />} />
-                <ItemCard title='User Master Data' icon={<AiOutlineFileText />} onClick={previewUser} />
-                {/* -------------------------------Modal---------------------------------- */}
-                <BranchModal open={openBranch} onClose={closeBranch} />
-                <DimensionModal open={openDimension} onClose={closeDimension} />
-                <UsersModal open={openUser} onClose={closeUser} />
-                <DocumentNumberingModal open={openDoc} onClose={closeDoc} />
+                <ItemCard title='User Master Data' icon={<AiOutlineFileText />} />
+
             </MainContainer>
         </>
     )
---
+++ src/presentations/systemInitialize/SystemInitialize.tsx
@@ -0,0 +1,45 @@
+import MainContainer from '@/components/MainContainer';
+import ItemCard from '@/components/card/ItemCart';
+import BranchModal from '@/components/modal/BranhModal';
+import React from 'react'
+import { AiOutlineFileSync, AiOutlineSolution, AiOutlineFileExclamation, AiOutlineFileText } from "react-icons/ai";
+import DimensionModal from './../../components/modal/DimensionsModal';
+import UsersModal from '@/components/modal/UsersModal';
+import DocumentNumberingModal from '@/components/modal/DocumentNumberingModal';
+
+export default function SystemInitializeMasterPage() {
+  const [openBranch, setOpenBranch] = React.useState(false);
+  const [openDimension, setOpenDimension] = React.useState(false);
+  const [openUser, setOpenUser] = React.useState(false);
+  const [openDoc, setOpenDoc] = React.useState(false);
+
+
+  const previewBranch = React.useCallback(() => setOpenBranch(true), []);
+  const closeBranch = React.useCallback(() => setOpenBranch(false), []);
+
+  const previewDimension = React.useCallback(() => setOpenDimension(true), []);
+  const closeDimension = React.useCallback(() => setOpenDimension(false), []);
+
+  const previewUser = React.useCallback(() => setOpenUser(true), []);
+  const closeUser = React.useCallback(() => setOpenUser(false), []);
+
+  const previewDoc = React.useCallback(() => setOpenDoc(true), []);
+  const closeDoc = React.useCallback(() => setOpenDoc(false), []);
+
+  return (
+    <>
+      <MainContainer title='Inventory'>
+        <ItemCard title='Branch' icon={<AiOutlineSolution />} onClick={previewBranch} />
+        <ItemCard title='Dimension' icon={<AiOutlineFileSync />} onClick={previewDimension} />
+        <ItemCard title='Document Numbering' icon={<AiOutlineFileExclamation />} onClick={previewDoc} />
+        <ItemCard title='User Licensing' icon={<AiOutlineFileText />} />
+        <ItemCard title='User Master Data' icon={<AiOutlineFileText />} onClick={previewUser} />
+        {/* -------------------------------Modal---------------------------------- */}
+        <BranchModal open={openBranch} onClose={closeBranch} />
+        <DimensionModal open={openDimension} onClose={closeDimension} />
+        <UsersModal open={openUser} onClose={closeUser} />
+        <DocumentNumberingModal open={openDoc} onClose={closeDoc} />
+      </MainContainer>
+    </>
+  )
+}
