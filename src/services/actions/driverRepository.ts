50ee8298bd9645ad211d00c402ebe7b3b3107cb0
Sopen <58549604+sopenlaz0@users.noreply.github.com>
2023-05-09 15:05:20 +0700

    fix update driver

--- src/services/actions/driverRepository.ts
+++ src/services/actions/driverRepository.ts
@@ -1,16 +1,75 @@
-import Repository from "@/astractions/repository";
-import request from "@/utilies/request";
-import Driver from "../../models/Driver";
+// import Repository from "@/astractions/repository";
+// import request from "@/utilies/request";
+// import Driver from "../../models/Driver";
 
+// export default class DriverRepository extends Repository<Driver> {
+//   url: string = "/EmployeesInfo?$filter=U_Driver eq 'Yes'";
+
+//   public static documentSerie = {
+//     Document: "1470000113",
+//   };
+
+//   async get<T>(query?: string): Promise<T[]> {
+//     const response: any = await request("GET", this.url)
+//       .then((res: any) => {
+//         const data = res?.data?.value?.map((e: any) => new Driver(e));
+//         return data;
+//       })
+//       .catch((e) => {
+//         throw new Error(e);
+//       });
+//     return response;
+//   }
+
+//   async find<T>(id: any): Promise<any> {
+//     const data = await request("GET", `${this.url}(${id})`)
+//       .then((res: any) => new Driver(res.data))
+//       .catch((e: Error) => {
+//         throw new Error(e.message);
+//       });
+
+//     return data;
+//   }
+
+//   async post(payload: any, isUpdate?: boolean, id?: any): Promise<any> {
+//     if (isUpdate)
+//       return await request(
+//         "PATCH",
+//         this.url + "(" + id + ")",
+//         Driver.toUpdate(payload)
+//       );
+
+//     return await request("POST", this.url, Driver.toCreate(payload));
+//   }
+
+//   async patch(id: any, payload: any): Promise<any> {
+//     return await request("PATCH", this.url, Driver.toUpdate(payload));
+//   }
+
+//   async delete(id: any): Promise<Driver> {
+//     throw new Error("Method not implemented.");
+//   }
+// }
+
+import BusinessPartner from '@/models/BusinessParter';
+import Repository from '../../astractions/repository';
+import request from '../../utilies/request';
+import BusinessPartnerRepository from './bussinessPartnerRepository';
+import { IContactPersonList } from '../../astractions/index';
+import Driver from '@/models/Driver';
 export default class DriverRepository extends Repository<Driver> {
-  url: string = "/EmployeesInfo?$filter=U_Driver eq 'Yes'";
+
+  url: string = '/EmployeesInfo';
+  query: string = "?$filter=U_Driver eq 'Yes'";
+
 
   public static documentSerie = {
-    Document: "1470000113",
-  };
+    Document: "22"
+  }
+
 
   async get<T>(query?: string): Promise<T[]> {
-    const response: any = await request("GET", this.url)
+    const response: any = await request("GET", this.url + this.query)
       .then((res: any) => {
         const data = res?.data?.value?.map((e: any) => new Driver(e));
         return data;
@@ -22,31 +81,29 @@ export default class DriverRepository extends Repository<Driver> {
   }
 
   async find<T>(id: any): Promise<any> {
-    const data = await request("GET", `${this.url}(${id})`)
-      .then((res: any) => new Driver(res.data))
+    const data = await request('GET', `${this.url}(${id})`).then((res: any) => new Driver(res.data))
       .catch((e: Error) => {
-        throw new Error(e.message);
-      });
+        throw new Error(e.message)
+      })
 
     return data;
   }
 
   async post(payload: any, isUpdate?: boolean, id?: any): Promise<any> {
-    if (isUpdate)
-      return await request(
-        "PATCH",
-        this.url + "(" + id + ")",
-        Driver.toUpdate(payload)
-      );
 
-    return await request("POST", this.url, Driver.toCreate(payload));
+    if (isUpdate) return await request('PATCH', this.url + "(" + id + ")", Driver.toUpdate(payload));
+
+    return await request('POST', this.url, Driver.toCreate(payload));
   }
+
 
   async patch(id: any, payload: any): Promise<any> {
-    return await request("PATCH", this.url, Driver.toUpdate(payload));
+    return await request('PATCH', this.url, Driver.toUpdate(payload));
   }
 
+
   async delete(id: any): Promise<Driver> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
+
 }
