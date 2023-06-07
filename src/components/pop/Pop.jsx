01c0f17cd343df370dec2da5cffeca4396ed90a6
sopencody <o.sophin29@gmail.com>
2023-06-07 10:26:08 +0700

    fix

--- src/components/pop/Pop.jsx
+++ src/components/pop/Pop.jsx
@@ -8,7 +8,7 @@ const PopCover = ({ open}) => {
   const collapse= useToggleStateContext()
   return (
     <div className={`w-[100%] h-[100%] ml-3 shadow-sm shadow-blue-500 rounded-sm flex items-center justify-center fixed z-50 opa ${open ? "block" : "hidden"} `}>
-      <div className={`${collapse ? "mr-[20.5rem]":"mr-[7rem]"}  inline-block text-white`}>
+      <div className={`${collapse ? "mr-[19.5%]":"mr-[7rem]"}  inline-block text-white`}>
         <CircularProgress color='inherit'/>
       </div>
     </div>
